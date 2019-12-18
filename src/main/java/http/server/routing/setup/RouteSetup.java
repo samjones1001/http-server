8d17fcdc05befbb23e3e88bc42054d1f5b3a78bb
Sam Jones <sam.jones494@googlemail.com>
2019-12-18 15:40:39 +0000

    further refactor of routing setup

--- src/main/java/http/server/routing/setup/RouteSetup.java
+++ src/main/java/http/server/routing/setup/RouteSetup.java
@@ -7,6 +7,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Map;
 
 public class RouteSetup {
     public static RequestRouter routerSetup() {
@@ -41,42 +42,56 @@ public class RouteSetup {
         }));
 
         router.addRoute("/poem", "GET", ((request, response) -> {
+            String body = readFileToString("./assets/html/poem.html");
             response.setStatus(200, "OK");
             response.addHeader("Content-Type", "text/html");
-            String body = readFile("./assets/html/poem.html");
             response.addBody(body.getBytes());
         }));
 
         router.addRoute("/letter", "POST", (((request, response) -> {
+            String body = insertValuesToHtml(readFileToString("./assets/html/letter.html"), request.getQueryParams());
             response.setStatus(200, "OK");
             response.addHeader("Content-Type", "text/html");
-            String body = readFile("./assets/html/letter.html");
-
-            for (String paramKey: request.getQueryParams().keySet()) {
-                String key = request.getQueryParams().get(paramKey).replace('%', ' ');
-                body = body.replaceAll("\\{" + paramKey + "}", key);
-            }
             response.addBody(body.getBytes());
         })));
 
         router.addRoute("/image", "GET", (((request, response) -> {
-            File image = new File("./assets/img/example.jpg");
+            byte[] bodyData = readFileToBytes(new File("./assets/img/example.jpg"));
             response.setStatus(200, "OK");
             response.addHeader("Content-Type", "image/jpg");
-            try {
-                response.addBody(Files.readAllBytes(image.toPath()));
-            } catch (IOException err) {}
+            response.addBody(bodyData);
         })));
 
         return router;
     }
 
-    private static String readFile(String path) {
+    private static byte[] readFileToBytes(File file) {
+        try {
+            return Files.readAllBytes(file.toPath());
+        } catch (IOException err) {
+            System.out.println(err.getMessage());
+            return null;
+        }
+    }
+
+    private static String readFileToString(String path) {
         try {
             return new String(Files.readAllBytes(Paths.get(path)));
         } catch (IOException err) {
             System.out.println(err.getMessage());
             return null;
         }
+    }
+
+    private static String insertValuesToHtml(String htmlTemplate, Map<String, String> queryParameters) {
+        for (String paramKey: queryParameters.keySet()) {
+            String paramValue = formatMultiWordParamValue(queryParameters.get(paramKey));
+            htmlTemplate = htmlTemplate.replaceAll("\\{" + paramKey + "}", paramValue);
+        }
+        return htmlTemplate;
+    }
+
+    private static String formatMultiWordParamValue(String paramkey) {
+        return paramkey.replace('%', ' ');
     }
 }
